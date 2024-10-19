2bfac7d8f7b1311aeb46d88b0f2f7b66354c061d
ArtHalilaj <122181817+ArtHalilaj@users.noreply.github.com>
2024-10-19 15:53:13 +0200

    menu

---
+++ includes/menu.php
@@ -0,0 +1,12 @@
+
+        <ul>
+            <img src="img/Mercedes-Benz-Logo.png" alt="">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="Categories.php">Categories</a></li>
+            <li><a href="AboutUs.php">About us</a></li>
+            <div style="display:flex;column-gap: 20px;">
+            <li><a style="color: green;border-bottom: 3px solid green;border-radius: 5px;padding: 5px;" href="LogIn.php">Log In</a></li>
+            <li><a style="color: blue;border-bottom: 3px solid blue;border-radius: 5px;padding: 5px;" href="signup.php">Sign Up</a></li>
+        </div>
+        </ul>
+    
